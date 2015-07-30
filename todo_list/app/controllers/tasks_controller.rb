02d64c074a2a5625e60da7ed987d264c059003d6
Andrea Coravos & dchen101 <pair+acoravos.dchen101@devbootcamp.com>
2015-07-30 12:01:11 -0700

    Add new task

--- todo_list/app/controllers/tasks_controller.rb
+++ todo_list/app/controllers/tasks_controller.rb
@@ -1,2 +1,16 @@
 class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create(description: params[:task][:description], completed: false)
+    redirect_to tasks_path
+  end
+
 end
